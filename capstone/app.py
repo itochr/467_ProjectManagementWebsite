8ddb12f3ea12d2330aa6b8169d19b34f8dd86445
tsao17 <tsao17@gmail.com>
2025-02-02 16:16:21 -0500

    Trying to make account creation page.
    need to figure out the db.execute_query

--- capstone/app.py
+++ capstone/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, json, jsonify
+from flask import Flask, render_template, json, jsonify, redirect
+from flask import request
+from flask_mysqldb import MySQL
 import os
 import database.db_connector as db
 
@@ -6,6 +8,7 @@ import database.db_connector as db
 
 app = Flask(__name__)
 db_connection = db.connect_to_database()
+mysql = MySQL(app)
 
 
 # Routes
@@ -31,6 +34,34 @@ def help():
 def projects():
     return render_template("projects.j2")
 
+@app.route('/account_creation', methods=['GET', 'POST'])
+def accountCreation():
+
+    if request.method == "POST":
+        # fire off i user presses Create Account
+        if request.form.get("Create_Account"):
+            # grab user form inputs
+            username = request.form["username"]
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+            password = request.form["password"]
+            accountTeam = "TeamA"
+            accountRole = "Developer"
+            
+
+            # account for null age AND homeworld
+            if username != "" and first_name != "" and last_name != "" and password != "":
+                # mySQL query to insert a new person into bsg_people with our form inputs
+                query = "INSERT INTO Accounts (accountUsername, accountFirstName, accountLastName, accountPassword, accountTeam, accountRole) VALUES (%s, %s, %s, %s, %s, %s)"
+                # cur = mysql.connection.cursor()
+                # cur.execute(query, (username, first_name, last_name, password, accountTeam, accountRole))
+                # mysql.connection.commit()
+                cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(first_name, last_name, password, accountTeam, accountRole))
+                results = cursor.fetchall()
+                return render_template("accounts.j2", accounts = results)
+
+    return render_template("account_creation.j2")
+
 @app.route('/accounts', methods=['GET', 'POST'])
 def accountAdmin():
     query = "SELECT * FROM Accounts;"
@@ -39,7 +70,6 @@ def accountAdmin():
     # results = json.dumps(cursor.fetchall())
     # return results
     return render_template("accounts.j2", accounts = results)
-
 
 # Listener
 if __name__ == "__main__":
