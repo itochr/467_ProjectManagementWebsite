53831f305111ba42c3e5dc2358cfc03f36ba3e0b
Vishwas Sastry <sastryv@oregonstate.edu>
2025-02-05 20:15:48 -0600

    Setup user Account authentication with MySQL database

--- capstone/app.py
+++ capstone/app.py
@@ -1,19 +1,51 @@
-from flask import Flask, render_template, json, jsonify, request, redirect
+from flask import Flask, render_template, json, jsonify, request, redirect, session, url_for
+import pymysql
 import os
 import database.db_connector as db
 
 # Configuration
 
 app = Flask(__name__)
+app.secret_key = 'sadfadsgdsfhs'
+# app.config["SESSION_PERMANENT"] = True
+# app.config["PERMANENT_SESSION_LIFETIME"] = 300
+app.config['SESSION_TYPE'] = 'filesystem'
+
+
+
 db_connection = db.connect_to_database()
+cursor = db_connection.cursor(pymysql.cursors.DictCursor)
 
 
 # Routes
-
 @app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
 def login():
-	return render_template("login.j2")
+	screenMsg = ''
+	if request.method == 'POST' and 'accountUsername' in request.form and 'password' in request.form:
+		accountUsername = request.form['accountUsername']
+		password = request.form['password']
+		query = 'SELECT * FROM Accounts WHERE accountUsername = %s AND accountPassword = %s'
 
+		# Citation: https://stackoverflow.com/a/47957611/18182802
+		inputs = (accountUsername, password)
+		cursor.execute(query, inputs)
+		user = cursor.fetchone()
+
+		# results = json.dumps(cursor.fetchone())
+		# return results
+		if user:
+			session['loggedin'] = True
+			session['accountID'] = user['accountID']
+			session['accountUsername'] = user['accountUsername']
+			# session['accountFirstName'] = user['accountFirstName']
+			screenMsg = 'Logged in successfully !'
+			accountFirstName = user['accountFirstName']
+			return render_template('main.j2', accountFirstName = accountFirstName)
+		else:
+			screenMsg = 'Please enter correct username / password!'
+		return render_template('login.j2', screenMsg = screenMsg)
+	return render_template("login.j2")
 
 @app.route('/main', methods=['GET', 'POST'])
 def main():
@@ -29,6 +61,30 @@ def help():
 
 @app.route('/projects', methods=['GET', 'POST'])
 def projects():
+	screenMsg = ''
+	if request.method == 'POST' and 'accountUsername' in request.form and 'password' in request.form:
+		accountUsername = request.form['accountUsername']
+		password = request.form['password']
+		query = 'SELECT * FROM Accounts WHERE accountUsername = %s AND accountPassword = %s'
+
+		# Citation: https://stackoverflow.com/a/47957611/18182802
+		inputs = (accountUsername, password)
+		cursor.execute(query, inputs)
+		user = cursor.fetchone()
+
+		# results = json.dumps(cursor.fetchone())
+		# return results
+		if user:
+			session['loggedin'] = True
+			session['accountID'] = user['accountID']
+			session['accountUsername'] = user['accountUsername']
+			# session['accountFirstName'] = user['accountFirstName']
+			screenMsg = 'Logged in successfully !'
+			accountFirstName = user['accountFirstName']
+			return render_template('main.j2', accountFirstName = accountFirstName)
+		else:
+			screenMsg = 'Please enter correct username / password!'
+		return render_template('login.j2', screenMsg = screenMsg)
 	return render_template("projects.j2")
 
 @app.route('/accounts', methods=['GET', 'POST'])
